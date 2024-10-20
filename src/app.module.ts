ddc233e273d7c617f814fdf54d5a304cc187ad63
Alison Simião <alison.s@multiplier.com.br>
2024-10-20 19:32:27 -0300

    exporta html

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
 import { SocketGateway } from './web-socket/web-socket.gateway';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { resolve } from 'path';
 
 @Module({
+  imports: [
+    ServeStaticModule.forRoot(
+      (() => {
+        const publicDir = resolve('./public');
+        const servePath = '/';
+
+        return {
+          rootPath: publicDir,
+          serveRoot: servePath,
+          exclude: ['/api*'],
+        };
+      })(),
+    )
+  ],
   providers: [SocketGateway],
 })
 export class AppModule {}
